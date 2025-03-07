ff250a60a2b0ead8b03037e377bf7c798b9a3d13
Ammar <ammar@localhost.localdomain>
2025-03-07 22:55:31 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rajhi-importer",
+    version="0.1.0",
+    description="An importer for Rajhi card PDF statements",
+    author="Ammar Shaqeel",
+    url="https://github.com/ammarshaqeew/rajhi-importer",
+    packages=find_packages(),
+    install_requires=[
+        "beancount>=3.0.0",
+        "pdfplumber>=0.7.0",
+        "python-dateutil>=2.8.0",
+    ],
+    python_requires=">=3.7",
+    keywords="beancount, finance, accounting, pdf, import",
+)
